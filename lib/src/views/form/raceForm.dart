1428d7a2972c71ac78a4ede93039a07411997559
EstefanyH <estefanyhuamani287@gmail.com>
2024-07-11 15:55:14 -0500

    update de UI traceView

--- lib/src/views/form/raceForm.dart
+++ lib/src/views/form/raceForm.dart
@@ -29,7 +29,7 @@ class RaceForm extends RaceViewModel {
         child: Column(
           children: [
             Expanded(
-              flex: 2,
+              flex: 1,
               child: Row(
                 children: [
                   Expanded(
@@ -50,65 +50,65 @@ class RaceForm extends RaceViewModel {
             ),
             Expanded(
               flex: 1,
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.white,
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0), 
-                        child: Column(
-                          children: [
-                            const Text('Te llevo ah..', style: titleStyleIndigo,),
-                            InkWell(
-                              child: TextFormField(
-                                  enabled: false,
-                                  decoration: const InputDecoration(
-                                    counterText: '',
-                                    labelText: 'Origen',
-                                    prefixIcon: Icon(Icons.gps_fixed, color: Colors.blueAccent,),
-                                    border: OutlineInputBorder(),
-                                  ),
-                                  readOnly: true
-                                ),
-                              onTap: () {
-                                goToMapView(); 
-                              },
-                            ),
-                            const SizedBoxH10(),
-                            InkWell(
-                              child: TextFormField(
-                                enabled: false,
-                                keyboardType: TextInputType.number,
-                                decoration: const InputDecoration(
-                                  counterText: '',
-                                  labelText: 'Llegada',
-                                  prefixIcon: Icon(Icons.map, color: Colors.blueAccent,),
-                                  border: OutlineInputBorder(),
-                                )
-                              ),
-                             onTap: () {
-                              goToMapView(); 
-                             },),                            
-                            const SizedBoxH10(),
-                            CupertinoButton(
-                              borderRadius: const BorderRadius.all(Radius.circular(10)),
-                              color: Colors.indigo,
-                              child: const Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                Text( 'Taxi...', 
-                                style: style16White,),
-                              ],), 
-                              onPressed: () {  }
-                            ),
-                          ],
+              child: Padding(
+                padding: EdgeInsets.all(15), 
+                child: Column(
+                  children: [
+                    const Text('Te llevo ah..', style: titleStyleIndigo,),
+                    InkWell(
+                      child: TextFormField(
+                          enabled: false,
+                          decoration: const InputDecoration(
+                            counterText: '',
+                            labelText: 'Origen',
+                            prefixIcon: Icon(Icons.gps_fixed, color: Colors.blueAccent,),
+                            border: OutlineInputBorder(),
+                          ),
+                          readOnly: true
                         ),
+                      onTap: () {
+                        goToMapView(); 
+                      },
+                    ),
+                    const SizedBoxH10(),
+                    InkWell(
+                      child: TextFormField(
+                        enabled: false,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          counterText: '',
+                          labelText: 'Llegada',
+                          prefixIcon: Icon(Icons.map, color: Colors.blueAccent,),
+                          border: OutlineInputBorder(),
+                        )
+                      ),
+                      onTap: () {
+                      goToMapView(); 
+                      },),                            
+                    const SizedBoxH10(),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        counterText: '',
+                        labelText: 'Monto',
+                        prefixIcon: Icon(Icons.payment, color: Colors.blueAccent,),
+                        border: OutlineInputBorder(),
                       )
                     ),
-                  ),
-                ],
+                    const SizedBoxH10(),
+                    CupertinoButton(
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      color: Colors.indigo,
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                        Text( 'Taxi...', 
+                        style: style16White,),
+                      ],), 
+                      onPressed: () {  }
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
